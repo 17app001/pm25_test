74911381f533129ed38884bc1d126758889c63d1
jerrytw0923 <mdream.studio@gmail.com>
2025-06-04 15:09:40 +0800

    更換成aiven資料庫

---
+++ app.py
@@ -0,0 +1,107 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+import pandas as pd
+from pm25 import (
+    get_pm25_data_from_mysql,
+    update_db,
+    get_pm25_data_by_site,
+    get_all_counties,
+    get_site_by_county,
+)
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route("/pm25-county-site")
+def pm25_county_site():
+    county = request.args.get("county")
+    sites = get_site_by_county(county)
+    result = json.dumps(sites, ensure_ascii=False)
+
+    return result
+
+
+@app.route("/pm25-site")
+def pm25_site():
+    counties = get_all_counties()
+    return render_template("pm25-site.html", counties=counties)
+
+
+@app.route("/pm25-data-site")
+def pm25_data_by_site():
+    county = request.args.get("county")
+    site = request.args.get("site")
+
+    if not county or not site:
+        result = json.dumps({"error": "縣市跟站點名稱不能為空!"}, ensure_ascii=False)
+    else:
+        columns, datas = get_pm25_data_by_site(county, site)
+        df = pd.DataFrame(datas, columns=columns)
+        # 轉換字串時間格式
+        date = df["datacreationdate"].apply(lambda x: x.strftime("%Y-%m-%d %H"))
+
+        data = {
+            "county": county,
+            "site": site,
+            "x_data": date.to_list(),
+            "y_data": df["pm25"].to_list(),
+            "higher": df["pm25"].max(),
+            "lower": df["pm25"].min(),
+        }
+
+        result = json.dumps(data, ensure_ascii=False)
+
+    return result
+
+
+# 更新資料庫
+@app.route("/update-db")
+def update_pm25_db():
+    count, message = update_db()
+    nowtime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    result = json.dumps(
+        {"時間": nowtime, "更新筆數": count, "結果": message}, ensure_ascii=False
+    )
+    return result
+
+
+@app.route("/")
+def index():
+    # 取得資料庫最新資料
+    columns, datas = get_pm25_data_from_mysql()
+    # 取出不同縣市給select
+    df = pd.DataFrame(datas, columns=columns)
+    # 排序縣市
+    counties = sorted(df["county"].unique().tolist())
+
+    # 選取縣市後的資料(預設ALL)
+    county = request.args.get("county", "ALL")
+
+    if county == "ALL":
+        df1 = df.groupby("county")["pm25"].mean().reset_index()
+        x_data = df1["county"].to_list()
+    else:
+        # 取得特定縣市的資料
+        df = df.groupby("county").get_group(county)
+        # 繪製所需資料
+        x_data = df["site"].to_list()
+
+    y_data = df["pm25"].to_list()
+    columns = df.columns.tolist()
+    datas = df.values.tolist()
+
+    return render_template(
+        "index.html",
+        columns=columns,
+        datas=datas,
+        counties=counties,
+        selected_county=county,
+        x_data=x_data,
+        y_data=y_data,
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ pm25.py
@@ -0,0 +1,168 @@
+import pandas as pd
+import pymysql
+
+
+def get_site_by_county(county):
+    conn = None
+    sites = []
+    try:
+        conn = open_db()
+        cur = conn.cursor()
+        sqlstr = "select distinct site from pm25 where county=%s;"
+        cur.execute(sqlstr, (county,))
+        datas = cur.fetchall()
+        print(datas)
+        sites = [data[0] for data in datas]
+
+    except Exception as e:
+        print(e)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return sites
+
+
+def get_all_counties():
+    conn = None
+    counties = []
+    try:
+        conn = open_db()
+        cur = conn.cursor()
+        sqlstr = "select distinct county from pm25;"
+        cur.execute(sqlstr)
+        datas = cur.fetchall()
+        print(datas)
+        counties = [data[0] for data in datas]
+
+    except Exception as e:
+        print(e)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return counties
+
+
+table_str = """
+            CREATE TABLE IF NOT EXISTS pm25 (
+                site VARCHAR(50),
+                county VARCHAR(50),
+                pm25 FLOAT,
+                datacreationdate DATETIME,
+                itemunit VARCHAR(10),
+                PRIMARY KEY (site, datacreationdate)
+            )
+        """
+
+
+# 更新資料庫
+def update_db():
+    api_url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=540e2ca4-41e1-4186-8497-fdd67024ac44&limit=1000&sort=datacreationdate%20desc&format=CSV"
+    sqlstr = """
+        insert ignore into pm25(site,county,pm25,datacreationdate,itemunit) 
+        values(%s,%s,%s,%s,%s)
+    """
+    row_count = 0
+    message = ""
+    try:
+        # 讀取最新的雲端資料
+        df = pd.read_csv(api_url)
+        df["datacreationdate"] = pd.to_datetime(df["datacreationdate"])
+        df1 = df.dropna()
+        values = df1.values.tolist()
+        # 寫入資料庫
+        conn = open_db()
+        cur = conn.cursor()
+        # 檢查建立資料庫
+        cur.execute(table_str)
+        conn.commit()
+        cur.executemany(sqlstr, values)
+        row_count = cur.rowcount
+        conn.commit()
+
+        print(f"更新{row_count}筆資料成功!")
+        message = "更新資料庫成功!"
+
+    except Exception as e:
+        print(e)
+        message = f"更新資料庫失敗:{e}"
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return row_count, message
+
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # 載入 .env 環境變數
+
+
+def open_db():
+    conn = None
+    try:
+        conn = pymysql.connect(
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT")),
+            user=os.getenv("DB_USER"),
+            passwd=os.getenv("DB_PASSWORD"),
+            db=os.getenv("DB_NAME"),
+        )
+    except Exception as e:
+        print("資料庫開啟失敗", e)
+
+    return conn
+
+
+# 取得縣市對應的site資料
+def get_pm25_data_by_site(county, site):
+    conn = None
+    columns, datas = None, None
+    try:
+        conn = open_db()
+        cur = conn.cursor()
+        sqlstr = "select * from pm25 where county=%s and site=%s;"
+        cur.execute(sqlstr, (county, site))
+        # 輸出資料表欄位
+        print(cur.description)
+        columns = [col[0] for col in cur.description]
+        # 實際的資料
+        datas = cur.fetchall()
+    except Exception as e:
+        print(e)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return columns, datas
+
+
+def get_pm25_data_from_mysql():
+    conn = None
+    columns, datas = None, None
+    try:
+        conn = open_db()
+        cur = conn.cursor()
+        sqlstr = "select * from pm25 where datacreationdate=(select MAX(datacreationdate) from pm25);"
+        cur.execute(sqlstr)
+        # 輸出資料表欄位
+        print(cur.description)
+        columns = [col[0] for col in cur.description]
+        # 實際的資料
+        datas = cur.fetchall()
+    except Exception as e:
+        print(e)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return columns, datas
+
+
+# 本地運行
+if __name__ == "__main__":
+    # update_db()
+    # print(get_pm25_data_by_site("新北市", "桃園"))
+    print(get_site_by_county("高雄市"))
